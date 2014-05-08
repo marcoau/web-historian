87dd538043188e33528636592027f6868a041cfd
Marco Au <marco.au.321@gmail.com>
2014-05-07 17:00:53 -0700

    Implemented serveAssets for GET requests

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,43 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
-// require more modules/folders here!
+var helpers = require("./http-helpers");
+
+
+var fs = require('fs');
+
+// closure functions 
+var getAction = function(req, res){
+  console.log(req.url);
+  var homeDir = __dirname + '/public/';
+  var targetFile = path.basename(req.url) || 'index.html';
+  var asset = homeDir + targetFile;
+  console.log('Now looking for asset: ' + asset);
+  helpers.serveAssets(res, asset);
+};
+
+var postAction = function(req, res){
+  helpers.sendResponse(res, 200, {}, 'Successful POST');
+};
+
+var optionsAction = function(req, res){
+  helpers.sendResponse(res, 200, {}, 'Successfully connected!');
+};
+
+var actions = {
+  'GET': getAction,
+  'POST': postAction,
+  'OPTIONS': optionsAction
+};
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  var action = req.method;
+  if(action){
+    actions[action](req, res);
+  }else{
+    //404;
+    helpers.send404(res);
+
+  }
+  //res.end(archive.paths.list);
 };
